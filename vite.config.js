ae37158c47a80c9cb41c60daf95b4a67b5a70637
Maor Aknin <maoraknin125@gmail.com>
2023-02-15 14:39:55 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/mr-bitcoin/" : "/",
+  base: '/mr-bitcoin/',
+  publicPath: process.env.NODE_ENV === "production" ? "/mr-bitcoin/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
+  
 })
